94ab7d7ce5244b925816858fd73c5dac29568b25
zangjl <zangjialu@outlook.com>
2023-05-17 21:55:28 +0800

    idea创建初始化

---
+++ src/test/java/com/zjl/college_parent/CollegeParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjl.college_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
