b857bfb191021b12ac9631d82156c507411d0ec5
zangjl <zangjialu@outlook.com>
2023-05-18 10:54:37 +0800

    完成统一返回结果

--- service/service_edu/src/main/java/com/zjl/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/zjl/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,14 @@
 package com.zjl.eduservice.controller;
 
 
+import com.zjl.commonutils.R;
 import com.zjl.eduservice.entity.EduTeacher;
 import com.zjl.eduservice.service.EduTeacherService;
 import com.zjl.eduservice.service.impl.EduTeacherServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +24,9 @@ import java.util.List;
  * @author zjl
  * @since 2023-05-18
  */
+
+
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -31,17 +39,23 @@ public class EduTeacherController {
     //1 查询讲师所有数据
     //restful风格
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
+    @ApiOperation(value = "所有讲师列表")
+    public R findAllTeacher() {
         //调用service查询所有
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
 
     //2逻辑删除讲师
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
-        return  teacherService.removeById(id);
+    @ApiOperation(value = "逻辑删除所有讲师")
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师id", required = true) @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag)
+            return R.ok();
+        else
+            return R.error();
     }
 }
 
